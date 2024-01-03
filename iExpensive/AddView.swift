c0017edbd4530eb239c85506f6a3f40fa60c9c36
n0m4da <nomad._@hotmail.com>
2024-01-03 15:22:19 -0700

    addview, listDelete

---
+++ iExpensive/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpensive
+//
+//  Created by Luis Rivera on 03/01/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
---
+++ iExpensive/listDelete.swift
@@ -0,0 +1,18 @@
+//
+//  listDelete.swift
+//  iExpensive
+//
+//  Created by Luis Rivera on 03/01/24.
+//
+
+import SwiftUI
+
+struct listDelete: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listDelete()
+}
