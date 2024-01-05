d20730b5aaa0f31c58cfd74ddc20d1b15604b174
n0m4da <nomad._@hotmail.com>
2024-01-04 22:51:04 -0700

    list delete

--- iExpensive/AddView.swift
+++ iExpensive/AddView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    @Environment(\.dismiss) var dismiss
+    
+    
+    var expenses: Expenses
+    let types = ["Business", "Personal"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            Form{
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type){
+                    ForEach(types, id:\.self){
+                        Text($0)
+                    }
+                }.pickerStyle(.automatic)
+                
+                
+                TextField("Amount", value: $amount, format: .currency(code: "MXN"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar{
+                Button("save"){
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+        
     }
 }
 
 #Preview {
-    AddView()
+    AddView(expenses: Expenses())
 }
--- iExpensive/listDelete.swift
+++ iExpensive/listDelete.swift
@@ -7,9 +7,109 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID() // not garante they will be unique but is high chance never repeated
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses{
+    var items = [ExpenseItem]() {
+        didSet{
+            if let encoded = try? JSONEncoder().encode(items){
+                UserDefaults.standard.setValue(encoded, forKey: "Items")
+            }
+     
+        }
+    
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items"){
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems){
+                items = decodedItems
+                return
+            }
+        }
+            items = []
+    }
+}
+
 struct listDelete: View {
+    //MARK: -
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+  
+    
+    //MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            var personalItems: [ExpenseItem] {
+                expenses.items.filter { $0.type == "Personal" }
+                }
+            var businesItems: [ExpenseItem]{
+                expenses.items.filter{$0.type == "Business"}
+            }
+            List {
+                Section {
+                    ForEach(expenses.items) { item in
+                        if item.type == "Business" {
+                            HStack {
+                                Text(item.name)
+                                    .font(.headline)
+
+                                Spacer()
+
+                                Text(item.amount, format: .currency(code: Locale.current.identifier ))
+                                    .foregroundStyle(item.amount < 10 ? .black
+                                                     : item.amount>20 ? .red: .blue)
+                            }
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                } header: {
+                    Text("Business Expenses")
+                }
+
+                Section {
+                    ForEach(expenses.items) { item in
+                        if item.type == "Personal" {
+                            HStack {
+                                Text(item.name)
+                                    .font(.headline)
+
+                                Spacer()
+
+                                Text(item.amount, format: .currency(code: Locale.current.identifier ))
+                                    .foregroundStyle(item.amount < 10 ? .black
+                                                     : item.amount>20 ? .red: .blue)
+                            }
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                } header: {
+                    Text("Personal Expenses")
+                }
+            }
+            .navigationTitle("iExpenses")
+            .toolbar{
+                Button("Add Expenses", systemImage: "plus"){
+//                    let expense = ExpenseItem(name: "Text", type: "Personal", amount: 5)
+//                    expenses.items.append(expense)
+                    showingAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpense){
+                AddView(expenses: expenses)
+            }
+        }
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
